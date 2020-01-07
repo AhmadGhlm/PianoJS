a763e134bbdb6294313030e0a2c07cd4eac86ac5
AhmadGDev <Ahmad.ghelem@gmail.com>
2020-01-07 13:29:02 +0100

    write script for EventListener

--- script.js
+++ script.js
@@ -3,4 +3,10 @@
 const white_keys = ["z", "x", "c", "v", "b", "n", "m"];
 const black_keys = ["s", "d", "g", "h", "j"];
 
-const keys = document.querySelector(".keys");
+const keys = document.querySelectorAll(".keys");
+const whiteKeys = document.querySelectorAll(".key.white");
+const blackKeys = document.querySelectorAll(".key.black");
+
+keys.forEach(key => {
+  key.addEventListener("click", () => playNote(key));
+});
